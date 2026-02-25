55df023688857427843b70fa15923c185b741676
Josh Fiquette <Joshf@bezeqint.co.il>
2026-02-25 03:10:45 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,19 @@
+
+# IPLocation
+
+## Overview
+IPLocation is a project for geolocation services based on IP addresses.
+
+## Features
+- IP-to-location mapping
+
+## Installation
+```bash
+docker-compose up --build
+```
+
+## Usage
+Browse to http://localhost:8000/?ip=[IP]
+
+## Configuration
+Update API key from https://ipgeolocation.io/ in .env file, example under .env.example
---
+++ app/main.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+from fastapi import FastAPI, Query
+from fastapi.responses import PlainTextResponse
+
+app = FastAPI()
+
+def get_ip_info(ip_address):
+    
+    base = Path(__file__).resolve().parent.parent  # project root
+    env_path = base / ".env"
+    load_dotenv(dotenv_path=str(env_path))
+    API_KEY = os.getenv("IPGEO_API_KEY")
+    headers = {}
+    url = f"https://api.ipgeolocation.io/v3/ipgeo?apiKey={API_KEY}&ip={ip_address}"
+    response = requests.request("GET", url, headers=headers, data={})
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+        return None
+
+@app.get("/", response_class=PlainTextResponse)
+def ip_info(ip: str = Query(..., description="IP Address")):
+    info = get_ip_info(ip)
+    if info is None:
+        return("IP information not found")
+    result = get_ip_info("91.128.103.196")
+    country_name = info["location"]["country_name"]
+    city_name = info["location"]["city"]
+    return f"{country_name}/{city_name}"
